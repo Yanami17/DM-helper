dc0619fc4f69395b5a996bd660c6359ae8313838
Yanami17 <vinny0044@gmail.com>
2026-02-21 18:22:40 +0800

    Advanced Table Displays

---
+++ SamplePlugin/Windows/Monster.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamplePlugin.Windows;
+
+public class Monster
+{
+    public string Name { get; set; } = string.Empty;
+    public int MaxHP { get; set; }
+    public int CurrentHP { get; set; }
+    public int DC { get; set; }
+}
